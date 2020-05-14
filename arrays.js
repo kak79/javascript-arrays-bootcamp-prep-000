c4bbea7096bb1223eb7a1415e23815aff5ab4b6f
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-14 12:20:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,23 +6,32 @@ function addElementToBeginningOfArray(array,element){
   return [element, ...array]
 }
 
+
+
 function destructivelyAddElementToBeginningOfArray(newArray,element){
   newArray.unshift(element);
   return newArray
 }
 
+
+
 function addElementToEndOfArray(newArray1,element){
   return [ ...newArray1, element]
 }
+
+
 
 function destructivelyAddElementToEndOfArray(newArray2,element){
   newArray2.push(element);
   return newArray2
 }
 
+
+
 function accessElementInArray(newArray3,index){
   console.log(newArray3[index])
 }
+
 
 
 function destructivelyRemoveElementFromBeginningOfArray(newArray4){
@@ -30,17 +39,24 @@ function destructivelyRemoveElementFromBeginningOfArray(newArray4){
   return newArray4
 }
 
+
+
 function removeElementFromBeginningOfArray(newArray5){
   return newArray5.slice(1)
 }
+
+
 
 function destructivelyRemoveElementFromEndOfArray(newArray6){
   newArray6.pop()
   return newArray6
 }
 
+
+
 function removeElementFromEndOfArray(newArray7){
-  newArray7.slice(0, newArray7.length - 1)
+  newArray7.slice(0, newArray7.length -1)
   return newArray7
 }
+
 
