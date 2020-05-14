f16d99fb6b1eaeb29bbe7606ab0c676d87369d32
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-14 12:41:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -55,6 +55,7 @@ function destructivelyRemoveElementFromEndOfArray(newArray6){
 
 
 function removeElementFromEndOfArray(newArray7){
+  newArray7.lastIndexOf
   newArray7.slice(0, [newArray7.length-1])
   return newArray7
 }
